978ff68c5eb69f40ec32b4862af9858f2ac5b3ae
ddelah-lab <derek.a.delahoussaye@gmail.com>
2022-06-18 09:26:47 -0500

    Update - Terraform Providers for environment

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-#test
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-1"
+}
+
+resource "aws_instance" "app_server1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
